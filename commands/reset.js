26f6bc601b03119297b7164fadbbbf28852f78e2
martinirp <lucasrezende672@hotmail.com>
2026-06-26 02:58:21 -0300

    Adiciona mais logs no reset

--- commands/reset.js
+++ commands/reset.js
@@ -8,17 +8,17 @@ module.exports = {
         await sock.sendMessage(sender, { text: '🔄 Puxando as últimas atualizações do GitHub...' });
 
         exec('git pull', async (error, stdout, stderr) => {
+            console.log(`[RESET] Callback do exec disparado.`);
             if (error) {
-                console.error(`Erro ao executar git pull: ${error.message}`);
+                console.error(`[RESET] Erro ao executar git pull: ${error.message}`);
                 await sock.sendMessage(sender, { text: `❌ Falha ao atualizar o bot pelo GitHub:\n${error.message}` });
                 return;
             }
 
-            console.log(`[GIT PULL] ${stdout}`);
+            console.log(`[RESET] Saída do git: ${stdout}`);
             await sock.sendMessage(sender, { text: `✅ Atualização concluída com sucesso!\n\n${stdout}\n\nReiniciando o sistema agora... Aguarde uns segundos.` });
 
             // Dá um tempo para a mensagem ser enviada e encerra o processo
-            // O manager.js vai detectar que o bot fechou e vai abri-lo novamente com o código novo!
             setTimeout(() => {
                 console.log('[RESET] Encerrando o processo para reinicialização...');
                 process.exit(0);
